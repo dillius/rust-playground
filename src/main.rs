1cf0a0c59e08d4cb360c90f5fabc13df8cb3a888
Michael Henderson <dillius@dillius.com>
2024-05-11 12:39:50 -0400

    Max Profit exercise

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ mod vector;
 fn main() {
     // println!("Hello, world!");
 
-    println!("Result: {}", Vector::remove_duplicates(&mut vec![1,1,2]));
+    // println!("Result: {}", Vector::remove_duplicates(&mut vec![1,1,2]));
+    println!("Result: {}", Vector::max_profit2(vec![7,1,5,3,6,4]));
 }
--- src/vector.rs
+++ src/vector.rs
@@ -15,4 +15,32 @@ impl Vector {
         return pos;
     }
 
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut index = 0;
+        let mut min_buy_price = prices[0];
+        let mut max_sell_price = prices[0];
+        let mut total_profit = 0;
+        while index < prices.len() - 1 {
+            while (index < prices.len() - 1) && (prices[index] >= prices[index + 1]) {
+                index += 1
+            }
+            min_buy_price = prices[index];
+            while (index < prices.len() - 1) && (prices[index] <= prices[index + 1]) {
+                index += 1
+            }
+            max_sell_price = prices[index];
+            total_profit += max_sell_price - min_buy_price;
+        }
+        return total_profit;
+    }
+
+    pub fn max_profit2(prices: Vec<i32>) -> i32 {
+        let mut total_profit = 0;
+        for index in 1..prices.len() {
+            if(prices[index] > prices[index - 1]) {
+                total_profit += prices[index] - prices[index - 1];
+            }
+        }
+        return total_profit;
+    }
 }
